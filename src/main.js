00d162abab542009b1c615a343c27b4ea86e75d5
苏杨 <suyang_5070@126.com>
2019-03-19 21:50:13 +0800

    '拆分Header组件'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App'
 
 import router from './router'
+import Header from './components/Header/Header'
+
+// 注册全局组件
+Vue.component('Header', Header)
 
 new Vue({
   el:'#app',
